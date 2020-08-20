db50f90c240388563c70d9ac6c397c6ece043f3c
AsLuq <69976290+AsLuq@users.noreply.github.com>
2020-08-20 17:03:25 +0200

    project created

---
+++ Clienti/src/main/java/clienti/Clienti.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clienti;
+
+/**
+ *
+ * @author luqmanasghar
+ */
+public class Clienti {
+    public static void main(String args[]){
+    
+    }
+}
---
+++ Ristoratori/src/main/java/ristoratori/Ristoratori.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ristoratori;
+
+/**
+ *
+ * @author luqmanasghar
+ */
+public class Ristoratori {
+    public static void main(String args[]){
+    
+    }
+}

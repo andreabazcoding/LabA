e0dcfb8c89c8dd0387e8a385eb5f71ccc0f1722c
Luqman Asghar <abtluq@gmail.com>
2020-08-24 20:43:44 +0200

    parser read from file

--- Clienti/src/main/java/clienti/Parser.java
+++ Clienti/src/main/java/clienti/Parser.java
@@ -1,7 +1,7 @@
 package clienti;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -12,55 +12,35 @@ public class Parser {
 
     public Parser() {}    
     
-    public void readFromFile(String path) throws Exception
+    /**
+     * Read the specified clients files from the given path
+     * 
+     * @author Luqman Asghar
+     * @param path path of the file
+     * @return list of clients
+     */
+    public List<Cliente> readFromFile(String path) throws Exception
     {
         File file = new File(path);        
         Cliente cliente = new Cliente();
         Scanner scanner = new Scanner(file);
+        List<Cliente> listCli = new ArrayList<>();
         
-        System.out.println("------------------");
-        System.out.println("line scanner");
-        System.out.println("------------------");        
-        
-        while (scanner.hasNextLine()) {
-            Scanner line = new Scanner(scanner.nextLine());
-            cliente.setName((line.hasNext() && (!"-".equals(line.next())) ) ? line.next() : null);
-            cliente.setSurname((line.hasNext() && (!"-".equals(line.next()))) ? line.next() : null);
-            cliente.setCity((line.hasNext() && (!"-".equals(line.next()))) ? line.next() : null);
-            cliente.setProvince((line.hasNext() && (!"-".equals(line.next()))) ? line.next() : null);
-            cliente.setMail((line.hasNext() && (!"-".equals(line.next()))) ? line.next() : null);
-            cliente.setNickName((line.hasNext() && (!"-".equals(line.next()))) ? line.next() : null);
-            cliente.setPassword((line.hasNext() && (!"-".equals(line.next()))) ? line.next() : null);
-            line.close(); // teoricamente endline
-        }
-        scanner.close();
-        cliente.printCliente();
-        
-        System.out.println("------------------");
-        System.out.println("Scanner con substring");
-        System.out.println("------------------");
-        
-        cliente = new Cliente();
-        scanner = new Scanner(file);
         while (scanner.hasNextLine()) {
           String line = scanner.nextLine();
-          cliente.setName(line.substring(0, 29));
-          cliente.setSurname(line.substring(30, 59));
-          cliente.setCity(line.substring(60, 99));
-          cliente.setProvince(line.substring(100, 102));
-          cliente.setMail(line.substring(103, 134));
-          cliente.setNickName(line.substring(135, 145));
-          cliente.setPassword(line.substring(146, 160));
+          cliente.setName(line.substring(0, 29).trim());
+          cliente.setSurname(line.substring(29, 59).trim());
+          cliente.setCity(line.substring(59, 99).trim());
+          cliente.setProvince(line.substring(99, 102).trim());
+          cliente.setMail(line.substring(102, 134).trim());
+          cliente.setNickName(line.substring(134, 155).trim());
+          cliente.setPassword(line.substring(155, 170).trim());
+          
+          listCli.add(cliente);
         }
-        cliente.printCliente();
         scanner.close();
         
-        System.out.println("------------------");
-        System.out.println("Scanner last");
-        System.out.println("------------------");
-        scanner = new Scanner(file);
-        System.out.println(scanner.nextLine());
-        scanner.close();
+        return listCli;
     } 
                 
 }
